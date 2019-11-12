e47d3d8b04b51800401aeabc5a1e9b46e6ed0deb
Ken Goad <goadke@g.cofc.edu>
2019-11-12 15:37:33 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+attr_accessor :name
+attr_accessor :breed
+
+def initialize(name, breed = "Mutt")
+  @name = name
+end
+
+def breed(breed="Mutt")
+  @breed = breed(breed="Mutt")
+  return "#{@breed="Mutt"}"
+end
+
+end
